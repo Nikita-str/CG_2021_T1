8df730f4682d63a250aed0acc472da5084c2ac4f
name <newmynamespace@gmail.com>
2021-03-01 00:31:00 +0300

    now norm

--- MapObj.cpp
+++ MapObj.cpp
@@ -11,39 +11,14 @@ void MapObj::DrawItems(Image &canvas)
     E_Dir pl_dir = Player::Get().GetCurDir();
     auto pl_c = Player::Get().GetCenter();
 
-    Point check_p_0 {0,0};
-    Point check_p_1 {0,0};
-    Point check_p_2 {0,0};
-    Point check_p_3 {0,0};
-
-    Point pl_p_min {0,0};
-    Point pl_p;
+    Point pl_check_min {0,0};
+    Size pl_check_sz {15,15};
 
     switch (pl_dir) {
-    case E_Dir::UP:
-        check_p_0 = {.x = (pl_c.x - 10), .y = pl_c.y};
-        check_p_1 = {.x = (pl_c.x + 10), .y = check_p_0.y};
-        check_p_2 = {.x = check_p_0.x, .y = (pl_c.y + 20)};
-        check_p_3 = {.x = check_p_1.x, .y = check_p_2.y};
-        break;
-    case E_Dir::DOWN:
-        check_p_0 = {.x = (pl_c.x - 10), .y = pl_c.y};
-        check_p_1 = {.x = (pl_c.x + 10), .y = check_p_0.y};
-        check_p_2 = {.x = check_p_0.x, .y = (pl_c.y - 20)};
-        check_p_3 = {.x = check_p_1.x, .y = check_p_2.y};
-        break;
-    case E_Dir::LEFT:
-        check_p_0 = {.x = (pl_c.x), .y = (pl_c.y - 10)};
-        check_p_1 = {.x = check_p_0.x, .y = (pl_c.y + 10)};
-        check_p_2 = {.x = (pl_c.x + 20), .y = check_p_0.y };
-        check_p_3 = {.x = check_p_2.x, .y = check_p_1.y};
-        break;
-    case E_Dir::RIGHT:
-        check_p_0 = {.x = (pl_c.x), .y = (pl_c.y - 10)};
-        check_p_1 = {.x = check_p_0.x, .y = (pl_c.y + 10)};
-        check_p_2 = {.x = (pl_c.x - 20), .y = check_p_0.y};
-        check_p_3 = {.x = check_p_2.x, .y = check_p_1.y};
-        break;
+    case E_Dir::UP: pl_check_min = {.x = pl_c.x - 10, .y = pl_c.y - 12}; pl_check_sz.w += 5; break;
+    case E_Dir::DOWN: pl_check_min = {.x = pl_c.x - 10, .y = pl_c.y - 16}; pl_check_sz.w += 5; break;
+    case E_Dir::LEFT: pl_check_min = {.x = pl_c.x - 15, .y = pl_c.y - 10}; pl_check_sz.h += 5; break;
+    case E_Dir::RIGHT: pl_check_min = {.x = pl_c.x + 5, .y = pl_c.y - 10};  pl_check_sz.h += 5; break;
     default:break;
     }
 
@@ -51,8 +26,7 @@ void MapObj::DrawItems(Image &canvas)
     for (int i = 0; i < items.size(); i++) {
         auto &itm = items[i];
         if (check_points) {
-            if (itm.IsPointIn(check_p_0) || itm.IsPointIn(check_p_1) || 
-                itm.IsPointIn(check_p_2) || itm.IsPointIn(check_p_3)) {
+            if (itm.IsRectIn(pl_check_min, pl_check_sz)) {
                 parent.Draw_E(canvas, itm.pos); 
                 check_points = false; 
             }
